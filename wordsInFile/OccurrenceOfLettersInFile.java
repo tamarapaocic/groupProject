5a608e2c0003079fdff0bad96377eaf9fb2c8a25
Tamara Paocic <tamara.paocic@hotmail.com>
2017-06-02 00:44:20 +0200

    added occurrence of letters in file

--- wordsInFile/OccurrenceOfLettersInFile.java
+++ wordsInFile/OccurrenceOfLettersInFile.java
@@ -1,11 +1,32 @@
 package wordsInFile;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class OccurrenceOfLettersInFile {
 
 	public OccurrenceOfLettersInFile(ArrayList<String> fileContent){
 		
-		
+		Map<Character, Integer> treeMap = new TreeMap<Character, Integer>();
+		for (char i = 'a'; i <= 'z'; i++) {
+		    treeMap.put(i, 0);
+		}
+
+		char[] alphabet = fileContent.toString().toCharArray();
+
+		for (int i = 0; i < alphabet.length; i++) {
+		    char ch = Character.toLowerCase(alphabet[i]);
+		    if (treeMap.containsKey(ch)) {
+		        treeMap.put(ch, treeMap.get(ch) + 1);
+		    }
+		}
+        System.out.println("- Occurrence of letters: ");
+		for (char key : treeMap.keySet()) {
+		    int count = treeMap.get(key);
+		    if (count > 0) {
+		        System.out.println("  " + key + ":" + treeMap.get(key));
+		    }
+		}
 	}
-	}
+}
